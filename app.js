89aacd76136b7c908bba862c7a6201c96be4ee8a
William <william.ingo@skola.taby.se>
2023-04-27 17:46:58 +0200

    Movement och projectile.

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const gravity = {x: 0, y: 0.1};
 penn.fillStyle = "black";
 penn.fillRect(0, 0, canvas.width, canvas.height);
 
+
+
+
 // Intervall och hastighet av kulor.
 let speedX = 5; // Horizontal speed.
 let speedY = 7; // Vertical speed.
@@ -21,14 +24,17 @@ let speedY = 7; // Vertical speed.
 const MOVE_SPEED = 2;
 
 class player{
-    constructor(name, color, width, height, posX, posY, velocity, health, shield, weapon) {
+    constructor(name, color, imagesrc, width, height, posX, posY, yvelocity, xvelocity, health, shield, weapon) {
         this.name = name;
         this.color = color;
+        this.image = new Image();
+        this.image.src = imagesrc;
         this.width = width;
         this.height = height;
         this.posX = posX;
         this.posY = posY;
-        this.velocity = velocity;
+        this.yvelocity = yvelocity;
+        this.xvelocity = xvelocity;
         this.health = health;
         this.shield = shield;
         this.weapon = weapon;
@@ -49,16 +55,17 @@ class enemy{
     }
 }
 
-let sphere = {
+let bullet = {
     color: "red",
-    radius: 20,
-    posX: 95,
-    posY: canvas.mid_height - (10),
+    radius: 10,
+    posX: 0,
+    posY: 0,
 }
 
+
 // Deklarera spelaren och andra klasser.
-let player1 = new player("artistan", "red", 20, 60, canvas.height - 60, 20, 0, 100, 0, false);
-let enemy1 = new enemy("cucckck", "red", 20, 60, canvas.height - 60, 20, 0, 100, 0);
+let player1 = new player("artistan", "red", 'Images/Greed2.png', 30, 60, canvas.height - 60, 20, 0, 0, 100, 0, false);
+let enemy1 = new enemy("cucckck", "red", 30, 60, canvas.height - 60, 20, 0, 100, 0);
 
 
 // Define keys and an array to keep key states
@@ -68,7 +75,7 @@ const KEY_UP = 'ArrowUp';
 const KEY_DOWN = 'ArrowDown';
 const KEY_LEFT = 'ArrowLeft';
 const KEY_RIGHT = 'ArrowRight';
-const KEY_JUMP = "Space";
+const KEY_SPACE = "Space";
 
 // Create a logging function
 const keyEventLogger =  function (e) { 
@@ -92,11 +99,12 @@ function executeMoves(object) {
     if (keyState[KEY_RIGHT]) {      
         object.posX += speedX;
     }
-    if (keyState[KEY_JUMP]) {
-        object.posY -= speedY;
+    if (keyState[KEY_SPACE]) {
+        drawProjectile(bullet);
+        bullet.posX += speedX;
     }
 }
-
+ 
 // Funktioner för klockan vid toppen av fönstret.
 function startTime() {
     const today = new Date();
@@ -110,9 +118,7 @@ function startTime() {
 }
 
 function checkTime(i) {
-    if (i < 10) {
-        i += "0";
-    }; // lägger till 0 framför siffror < 10
+    if (i < 10) {i = "0" + i}; // lägger till 0 framför siffror < 10
     return i;
 }
 
@@ -121,34 +127,46 @@ function drawPlayers(object) {
     penn.fillRect(object.posX, object.posY, object.width, object.height);
 }
 
+const drawImage = (object) => {
+    const image = new Image();
+    image.src = object.image.src;
+    image.onload = () => {
+        penn.drawImage(image, object.posX, object.posY, 200, 200);
+    }
+}
+
 function isAlive(object) {
     if (object.health <= 0) {
-        return true
-    }
-    else {
         alert('YOU ARE DEAD!')
         return false
+    }
+    else {
+        return true
     }
 }
 
 function animateGravity(object) {
-    object.velocity += gravity.y;
-    object.posY += object.velocity;
+    object.yvelocity += gravity.y;
+    object.xvelocity += gravity.x;
+    object.posX += object.xvelocity
+    object.posY += object.yvelocity;
     const g = ground - object.height; // adjust for size
     if(object.posY >= g) {  
         object.posY = g - (object.posY - g); // 
-        object.velocity = -Math.abs(object.velocity) * bounce;
+        object.yvelocity = -Math.abs(object.yvelocity) * bounce;
     }
 }
 
 function animateRestForce(object) {
-
+    if (object.velocity > 0) {
+        // Do something. T.ex momentum after a movement.
+    }
 }
 
-function drawBall(ball) {
-    penn.fillStyle = ball.color;
+function drawProjectile(object) {
+    penn.fillStyle = object.color;
     penn.beginPath();
-    penn.arc(ball.posX, ball.posY, ball.radius, 0, 2 * Math.PI);
+    penn.arc(player1.posX + player1.width, player1.posY + (player1.height/2), object.radius, 0, 2 * Math.PI);
     penn.closePath();
     penn.fill();
 }
@@ -187,12 +205,25 @@ function clearCanvas() {
 // Det här är huvudfunktionen som kör funktioner för att animeringen ska fungera.
 // mainLoop har alla funktioner i sig, för att effektivisera strukturen och funktionen av de tillsammans.
 function mainLoop() {
-    executeMoves(player1)
-    clearCanvas()
-    drawPlayers(player1)
-    animateGravity(player1)
-    collisionControl(player1)
-    requestAnimationFrame(mainLoop);
+    if (player1.health > 0) {
+        executeMoves(player1)
+        clearCanvas()
+        // drawImage(player1)
+        drawPlayers(player1)
+        animateGravity(player1)
+        collisionControl(player1);
+        collisionControl(bullet);
+        requestAnimationFrame(mainLoop)
+    }
+    else {
+        penn.font = "'30px Cormorant Garamond', serif";
+        penn.fillText('YOU ARE DEAD!', 0 , ceiling);
+        penn.fillText('Press SPACE to Restart', 0 , ground);
+        document.addEventListener('keydown', keyEventLogger);
+        if (keyState[KEY_SPACE]) {
+            player1.health = 100;
+        }
+    }
 }
 
 // gameStartBtn är en knapp, som kör huvudfunktionen om den klickas.
